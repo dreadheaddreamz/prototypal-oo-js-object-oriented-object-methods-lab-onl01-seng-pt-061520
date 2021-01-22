f8a8e954ac449220f2c5cc449f33909768376b98
James Batsell <jbfuturedev@gmail.com>
2021-01-22 09:03:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ function BoardMember(name, homeState, training) {
     return "I like to help people."
   }
   BoardMember.prototype.releasePressStatement = function() {
-    return ""
+    return "You will see great things from Scuber."
+  }
+  
+  BoardMember.prototype.sayHi = function () {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`
   }
 }
