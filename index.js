97aff7ace650ed44ddabc05b6db4261a8cd09dd7
James Batsell <jbfuturedev@gmail.com>
2021-01-22 08:58:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function BoardMember(name, homeState, training) {
   this.homeState = homeState
   this.training = training
   
-  BoardMember.prototype.veto = function {
-    
+  BoardMember.prototype.veto = function() {
+    return "No, I must disagree"
   }
 }
